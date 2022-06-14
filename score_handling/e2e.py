724ffa4e4ddda6831ec7a9ba4d2bc17994426e23
Egozi888 <104723199+Egozi888@users.noreply.github.com>
2022-06-14 16:29:54 +0300

    test file complete and working

--- score_handling/e2e.py
+++ score_handling/e2e.py
@@ -4,24 +4,33 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-my_driver = webdriver.Chrome(executable_path="/Users/danielbenharosh/Downloads/chromedriver") 
-my_driver.get('http://127.0.0.1:30000/Score_Read')
-element =  my_driver.find_element_by_id('score').text
+range_1 = range(1, 1000)
 
-# print("Value of input box: " + element.get_attribute('value'))
-# print(element.get_attribute('80'))
-print (element)
+def test_scores_service():
+    my_driver = webdriver.Chrome(executable_path="/Users/danielbenharosh/Downloads/chromedriver") 
+    my_driver.get('http://127.0.0.1:30000/Score_Read')
+    element =  my_driver.find_element_by_id('score').text
+    # print (element)
+    element = int(element)
+    if element in range_1:
+        return 1
+    else:
+        return 0     
 
-element = int(element) + 4
 
-print (element)
     
 
-# def test_scores_service():
+test_score = test_scores_service()
+print(bool(test_score))
 
 
-
-
+def main_function():
+    if test_score == True:
+        print(0)
+    else:
+        print(-1)    
+    
+main_function()
 
 # urls = ['http://www.ynet.co.il','https://mako.co.il']
 # my_driver.get(urls[0])
